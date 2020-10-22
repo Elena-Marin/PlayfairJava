441dfe4fbee1973727f2e34c1e293b7978e9b393
Elena Marin <elenamarin@yahoo.com>
2020-10-22 09:56:15 +0300

    add some changes

--- src/Playfair.java
+++ src/Playfair.java
@@ -8,22 +8,22 @@ public class Playfair {
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
+        String alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
 
         System.out.print("Enter text to be encrypted: ");
         String textForEncryption = console.nextLine();
 
-        String textFormated = formatText(textForEncryption);
-        System.out.println("Prepared text: " + textFormated);
+        String textFormatted = formatText(textForEncryption);
+        System.out.println("Formatted text: " + textFormatted);
 
-        ArrayList<String> textPrepared = prepareText(textFormated);
+        ArrayList<String> textPrepared = prepareText(textFormatted);
         System.out.println(textPrepared);
 
         System.out.println("Enter encryption key: ");
         String encryptionKey = console.nextLine();
 
-        String alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
-        String encryptionKeyPrepared = prepareEncyptionKey(encryptionKey + alphabet);
-        System.out.println("Prepared text for encryption key: " + encryptionKeyPrepared);
+        String encryptionKeyPrepared = prepareEncryptionKey(encryptionKey + alphabet);
+        System.out.println("Formatted text for encryption key: " + encryptionKeyPrepared);
 
         String[][] keyMatrix = tableMatrix(encryptionKeyPrepared);
 
@@ -37,7 +37,7 @@ public class Playfair {
     }
 
     //Transform text in uppercase + remove all special characters and numbers + replace J with I
-    //Used for preparing the text and the encryption key
+    //Used for preparing the text to be encrypted and also for preparing the encryption key
     public static String formatText(String textForEncryption){
         textForEncryption = textForEncryption.toUpperCase();
         textForEncryption = textForEncryption.replaceAll("\\s+","Q");
@@ -49,19 +49,19 @@ public class Playfair {
     }
 
     //Transform the text into pairs and create an array for easy handling
-    //If there are groups of 2 characters with the same letter, replace the last one with "Z"
-    //If the last group of characters is odd, then insert "X" as last character
-    public static ArrayList<String> prepareText(String textFormated){
+    //If there are groups of 2 characters with the same letter, replace the last one with "Y"
+    //If the last group of characters is odd, then insert "Q" as last character
+    public static ArrayList<String> prepareText(String textFormatted){
 
         ArrayList<String> textArray = new ArrayList<>();
 
         int i = 0;
-        while(i < textFormated.length()){
+        while(i < textFormatted.length()){
 
             int j = i + 2;
-            if(j > textFormated.length())
-                j = textFormated.length();
-            textArray.add(textFormated.substring(i,j));
+            if(j > textFormatted.length())
+                j = textFormatted.length();
+            textArray.add(textFormatted.substring(i,j));
             i = j;
         }
 
@@ -85,9 +85,9 @@ public class Playfair {
 
     //Format the encryption key text
     //Remove the duplicates and insert the rest of the letters from alphabet
-    public static String prepareEncyptionKey(String encyptionKey){
+    public static String prepareEncryptionKey(String encryptionKey){
 
-        String inputEncryptionKey = formatText(encyptionKey);
+        String inputEncryptionKey = formatText(encryptionKey);
         StringBuilder outputEncyptionKey = new StringBuilder();
 
         for(int i = 0; i < inputEncryptionKey.length(); i++){
@@ -115,7 +115,8 @@ public class Playfair {
         return key;
     }
 
-    // searches matrix for char index
+    // Searches matrix for char index
+    //It will return the row and the column of the character from the matrix
     public static int[] findRowColumn (String character){
         int[] pair = {-1,-1}; // row,column
 
@@ -135,41 +136,43 @@ public class Playfair {
         return pair;
     }
 
-    //encrypt/decrypt the text prepared using the encryption key
+    //Encrypt/Decrypt the text prepared using the encryption key
     public static ArrayList<String>  encryptDecrypt(String type, ArrayList<String> textPrepared){
-        int[] fChar;
-        int[] sChar;
+        int[] firstChar;
+        int[] secondChar;
 
         ArrayList<String> converted = new ArrayList<>(); //(ArrayList<String>) (textPrepared.clone());
 
         int mod = 0;
 
-        if (type.equals("e")){
+        if (type.equals("e")){ //"e" used for encryption
             mod = 1;
         }
-        else if(type.equals("d")) {
+        else if(type.equals("d")) { //"d" used for decryption
             mod = 4;
         }
 
         for (String s : textPrepared) {
-            fChar = findRowColumn(String.valueOf(s.charAt(0)));
-            sChar = findRowColumn(String.valueOf(s.charAt(1)));
+            firstChar = findRowColumn(String.valueOf(s.charAt(0)));
+            secondChar = findRowColumn(String.valueOf(s.charAt(1)));
 
-            if (fChar[0] == sChar[0]) // same row
+            if (firstChar[0] == secondChar[0]) // same row
             {
-                converted.add(key[fChar[0]][(fChar[1] + mod) % 5] + key[sChar[0]][(sChar[1] + mod) % 5]);
-            } else if (fChar[1] == sChar[1]) // same column
+                converted.add(key[firstChar[0]][(firstChar[1] + mod) % 5] + key[secondChar[0]][(secondChar[1] + mod) % 5]);
+
+            } else if (firstChar[1] == secondChar[1]) // same column
             {
-                converted.add(key[(fChar[0] + mod) % 5][fChar[1]] + key[(sChar[0] + mod) % 5][sChar[1]]);
+                converted.add(key[(firstChar[0] + mod) % 5][firstChar[1]] + key[(secondChar[0] + mod) % 5][secondChar[1]]);
+
             } else // box
             {
-                converted.add(key[fChar[0]][sChar[1]] + key[sChar[0]][fChar[1]]);
+                converted.add(key[firstChar[0]][secondChar[1]] + key[secondChar[0]][firstChar[1]]);
             }
         }
     return converted;
     }
 
-    //after decryption, replace the z and x characters back
+    //After decryption, replace the Y and Q characters back
     public static ArrayList<String> prepareDecryptedText(ArrayList<String> decryptedText) {
         
         ArrayList<String> decryptedTextPrepared = (ArrayList<String>) decryptedText.clone();
@@ -195,7 +198,7 @@ public class Playfair {
         return decryptedTextPrepared;
     }
 
-    //covert an array list to String
+    //Covert an array list to String
     public static String arraylistToString (ArrayList < String > textToConvert) {
         StringBuilder textConverted = new StringBuilder();
         for (String s : textToConvert) {
